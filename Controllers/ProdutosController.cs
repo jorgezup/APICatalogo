ac3b91856ee1c31e30024475c44e20fc31a5a7c7
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-10-25 14:19:34 -0400

    feat: created controller with method get and get by id

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            if (_context.Produtos == null) return NotFound("Product not found");
+            
+            var produtos = _context.Produtos.ToList();
+            return Ok(produtos);
+
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            if (_context.Produtos == null) return NotFound("There's no product.");
+
+            var produto = _context.Produtos.FirstOrDefault(
+                p => p.ProdutoId == id);
+            if (produto is null)
+                return NotFound("Product not found");
+
+            return Ok(produto);
+        }
+
+    }
+}
