2d0153ead7e0f6327b1cb43464793398c61ebec6
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-10-25 11:23:16 -0400

    feat: added data annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,18 +1,38 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 //Classes Anêmicas - Classes que não tem comportamento - Classes que não tem métodos - Só possuem propriedades
 
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
     {
         Produtos = new Collection<Produto>(); //Inicializando a coleção de produtos
     }
-    
+    [Key]
     public int CategoriaId { get; set; }
+    
+    [Required]
+    [MaxLength(80)]
     public string? Nome { get; set; }
+    
+    [Required]
+    [MaxLength(300)]
     public string? ImagemUrl { get; set; }
     
     public ICollection<Produto>? Produtos { get; set; } //Relacionamento 1 para N
 }
+
+
+// DataAnnotations
+// key
+// table(name)
+// column(name)
+// required
+// stringlength
+// datatype
+// notmapped
+// foreignkey
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,13 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace APICatalogo.Models;
 //Classes Anêmicas - Classes que não tem comportamento - Classes que não tem métodos - Só possuem propriedades
 
+[Table("Produtos")]
 public class Produto
 {
+    [Key]
     public int ProdutoId { get; set; }
+    
+    [Required]
+    [MaxLength(80)]
     public string? Nome { get; set; }
+    
+    [Required]
+    [MaxLength(300)]
     public string? Descricao { get; set; }
+    
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Preco { get; set; }
+    
+    [Required]
+    [MaxLength(300)]
     public string? ImagemUrl { get; set; }
+    
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     
