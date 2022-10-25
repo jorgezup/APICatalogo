8896b743b4cbf425a6b247c000e38654e12b1e30
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-10-25 09:26:24 -0400

    feat: added service to use Database

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext using SQL Server Provider
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseMySql(connectionString, 
+    ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
