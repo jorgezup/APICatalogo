f84cab6de7844a9e77bb77c08defae85c476d46d
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-10-25 21:10:51 -0400

    feat: added try/catch to deal with some errors in ProductsController

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -24,62 +24,96 @@ namespace APICatalogo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            if (_context.Produtos == null) return NotFound("Product not found");
+            try
+            {
+                if (_context.Produtos == null) return NotFound("Product not found");
             
-            var produtos = _context.Produtos.AsNoTracking().ToList();
-            return Ok(produtos);
-
+                var produtos = _context.Produtos.AsNoTracking().ToList();
+                return Ok(produtos);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
         }
 
         [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
-            if (_context.Produtos == null) return NotFound("There's no product.");
+            try
+            {
+                if (_context.Produtos == null) return NotFound("There's no product.");
 
-            var produto = _context.Produtos.FirstOrDefault(
-                p => p.ProdutoId == id);
-            if (produto is null)
-                return NotFound("Product not found");
+                var produto = _context.Produtos.FirstOrDefault(
+                    p => p.ProdutoId == id);
+                if (produto is null)
+                    return NotFound("Product not found");
 
-            return Ok(produto);
+                return Ok(produto);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
         }
 
         [HttpPost]
         public ActionResult Post(Produto produto)
         {
-            _context.Produtos?.Add(produto);
-            _context.SaveChanges();
+            try
+            {
+                _context.Produtos?.Add(produto);
+                _context.SaveChanges();
 
-            return new CreatedAtRouteResult("ObterProduto", 
-                new { id = produto.ProdutoId }, produto);
+                return new CreatedAtRouteResult("ObterProduto", 
+                    new { id = produto.ProdutoId }, produto);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
         }
 
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, Produto produto)
         {
-            if (id != produto.ProdutoId)
-                return BadRequest();
+            try
+            {
+                if (id != produto.ProdutoId)
+                    return BadRequest();
 
-            _context.Entry(produto).State = EntityState.Modified;
-            _context.SaveChanges();
+                _context.Entry(produto).State = EntityState.Modified;
+                _context.SaveChanges();
 
-            return Ok(produto);
+                return Ok(produto);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
-            if (_context.Produtos == null) return NotFound("There's no product.");
+            try
+            {
+                if (_context.Produtos == null) return NotFound("There's no product.");
 
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+                var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
             
-            if (produto is null)
-                return NotFound("Product not found");
+                if (produto is null)
+                    return NotFound("Product not found");
             
-            _context.Produtos.Remove(produto);
-            _context.SaveChanges();
+                _context.Produtos.Remove(produto);
+                _context.SaveChanges();
 
-            return Ok(produto);
+                return Ok(produto);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
         }
         
     
